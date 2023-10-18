ca59546024a07456f57297a5cc83662d69e02584
TahaMirza50 <tahamirza50@gmail.com>
2023-10-18 15:39:33 +0500

    added auth and middleware

---
+++ Middleware/Auth.middleware.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateAdmin = (req,res,next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+        if(err) return res.sendStatus(403);
+        if (user.role != 'admin') return res.sendStatus(403);
+        
+        req.user = user;
+        next();
+    });
+};
+
+const authenticateUser = (req,res,next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+        if(err) return res.sendStatus(403);
+        if (user.role != 'user') return res.sendStatus(403);
+        
+        req.user = user;
+        next();
+    });
+};
+
+
+const authenticateRefresh = (req,res,next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err,user) => {
+        if(err) return res.sendStatus(403);
+        
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = { authenticateAdmin, authenticateUser, authenticateRefresh };
--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -2,7 +2,12 @@ const Router = require('express');
 
 const router = Router();
 const authController = require('../Controllers/Auth.controller');
+const authMiddleware = require('../Middleware/Auth.middleware');
 
-router.post('/register',authController.register);
+router.post('/register', authController.register);
+
+router.post('/login', authController.login);
+
+router.post('/getaccesstoken', authMiddleware.authenticateRefresh, authController.getAccessToken);
 
 module.exports = router;
