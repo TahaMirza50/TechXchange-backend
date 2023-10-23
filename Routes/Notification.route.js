cb70872eecbfc5dc98f5dd64c839ad21d8b9c7d0
Uzair Ali <uzairalinusrat@gmail.com>
2023-10-23 23:17:51 +0500

    made get notification and delete notification api

---
+++ Routes/Notification.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+const notificationController = require('../Controllers/Notification.controller')
+const authMiddleware = require('../Middleware/Auth.middleware')
+
+router.get('/', authMiddleware.authenticateUser ,notificationController.getNotificationBox)
+
+router.delete('/deletenotification/:notid', authMiddleware.authenticateUser,notificationController.deleteNotification)
+
+module.exports = router
